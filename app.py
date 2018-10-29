24e076711d2b242133e418a3fbc0bff4518534c9
Murali <muralismail4u@gmail.com>
2018-10-29 17:22:57 +0530

    chore(app): half boil

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template,request,redirect,url_for # For flask implementation
+from flask import Flask, render_template,request,redirect,url_for, jsonify # For flask implementation
 from pymongo import MongoClient # Database connector
 from bson.objectid import ObjectId # For ObjectId to work
+from bson import Binary, Code
+from bson.json_util import dumps
+import datetime
+from datetime import timedelta
+import pytz
+from constants import TIME_ZONE
 
 client = MongoClient('localhost', 27017)    #Configure the connection to the database
 db = client.arduinoDb    #Select the database
-todos = db.todo #Select the collection
-machines = db.machines #collection for storing list of machines
-# machineTimings = db.machineTimings
 
+machines = db.machines #collection for storing list of machines
+machineTimings = db.machineTimings
 app = Flask(__name__)
-title = "TODO with Flask"
-heading = "ToDo Reminder"
+
 #modify=ObjectId()
+now = datetime.datetime.now(pytz.timezone(TIME_ZONE))
 
 def redirect_url():
     return request.args.get('next') or \
@@ -20,41 +25,59 @@ def redirect_url():
 
 @app.route("/")
 def apiWelcome():
+	print(request.remote_addr)
 	return "this is api for our aurdino"
 
 @app.route("/ON")
 def machineOn():
-	machine = machines.find_one({"machinename":"arduino123"})
-
-	print(machine)
-
-	return machine
+	machines.update_one({"machinename":"arduino123"}, {"$set": {"startTime":now, "machineStaus": 1}}, upsert=True)
+	return "switched on"
 
 @app.route("/OFF")
 def machineOff():
-	print("machine data recieved")
-	return "hello"	
+	cursorMachine = machines.find_one({"machinename":"arduino123"})	
+	if cursorMachine["machineStaus"] != 0:
+		machines.update_one({"machinename":"arduino123"}, {"$set": {"stopTime":now,"machineStatus":0}}, upsert=True)
+		startTime = cursorMachine["startTime"]
+		print(startTime)
+		stopTime = cursorMachine["stopTime"]
+		time_difference = stopTime - startTime
+		time_difference_in_minutes = time_difference / timedelta(minutes=1)
+		print(time_difference_in_minutes)
+		if "elapsedTime" in cursorMachine:
+			elapsedTime = cursorMachine["elapsedTime"] + time_difference_in_minutes
+			print(elapsedTime)
+			machines.update_one({"machinename":"arduino123"}, {"$set": {"elapsedTime":elapsedTime}}, upsert=True)
+		return str(time_difference_in_minutes)	
 
 @app.route("/MACHINEPROBLEM")
 def machineProblem():
+	machines.update_one({"machinename":"arduino123"}, {"$set": {"machineStatus":3}}, upsert=True)
 	print("machine data recieved")
 	return "hello"
 
 @app.route("/IDLE")
 def machineIdle():
+	machines.update_one({"machinename":"arduino123"}, {"$set": {"machineStatus":4}}, upsert=True)
 	print("machine data recieved")
-	return "hello"
+	return "Machine idle"
 
 @app.route("/createmachine")
 def createmachine():
 	print("machine data recieved")
-	hello = machines.insert_one({"machinename": "arduino124", "machineStaus": 0, "totalTimeOn": 0 })
+	hello = machines.insert_one({"machinename": "arduino124", "machineStaus": 0, "totalTimeOn": 0,"elapsedTime": 0 })
 	print(hello.inserted_id)
 	return "hello"
 
+@app.route("/mob/getmachines")
+def getmachinesfn():
+	print("machine data recieved")
+	machine = machines.find()	
+	return dumps(machine)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8081)
 # Careful with the debug mode..
 
 
